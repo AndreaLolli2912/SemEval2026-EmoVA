b468bb12895319ca114ac5621c07b5762da93bb3
AndreaLolli2912 <andrea.lolli1@outlook.com>
2026-01-24 09:38:28 +0100

    fix: rename file

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,11 +1,14 @@
 from .encoder import TransformerEncoder
+from .set_attention import ISAB, PMA
 from .lstm import LSTMEncoder
 from .heads import PredictionHead
 from .affect_model import AffectModel, masked_mse_loss
 
 __all__ = [
     'TransformerEncoder',
-    'LSTMEncoder', 
+    'ISAB',
+    'PMA',
+    'LSTMEncoder',
     'PredictionHead',
     'AffectModel',
     'masked_mse_loss'
